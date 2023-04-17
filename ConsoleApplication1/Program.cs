a69935013e620bd2267dfb69ba3a357fa62a800f
sakovets <skystalker01@gmail.com>
2023-04-17 13:16:08 +0300

    comment

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введiть три числа");
+            float a = Convert.ToSingle(Console.ReadLine());
+            float b = Convert.ToSingle(Console.ReadLine());
+            float c = Convert.ToSingle(Console.ReadLine());
+            float m = Max(a, b, c);
+            Console.WriteLine("максимальне число:  " + m);
+            Console.ReadKey();
+        }
+        static float Max(float x, float y, float z)
+        {
+            if (x > y && x > z)
+            {
+                return x;
+            }
+            else if (y > x && y > z)
+            {
+                return y;
+            }
+            else
+            {
+                return z;
+            }
+
+        }
+    }
+}
